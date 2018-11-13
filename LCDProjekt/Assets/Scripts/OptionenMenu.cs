8ecbd64c688916c683aa45556ae331abf727bc4e
Erkin Altuntas <erkin98@live.de>
2018-11-13 10:38:10 +0100

    Add Project

---
+++ LCDProjekt/Assets/Scripts/OptionenMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionenMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    
+
+    public void setVolume(float lautstaerke)
+    {
+        audioMixer.SetFloat("lautstaerke", lautstaerke);
+        
+    }
+}
---
+++ LCDProjekt/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour {
+
+    public void ladeSpiel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
