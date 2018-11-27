7120d63a17ce9e5d95510ca19ec3638f0455d3f1
Erkin Altuntas <erkin98@live.de>
2018-11-27 12:11:21 +0100

    dialog effekt

---
+++ LCDProjekt/Assets/Scripts/DialogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour {
+    public float delay = 0.005f;
+    public string fullText;
+    private string currentText = "";
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(ShowText());
+    }
+
+    private IEnumerator ShowText()
+    {
+        for(int i=0; i<fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+
+
+}
