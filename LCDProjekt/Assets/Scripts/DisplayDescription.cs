1bae6230dc4ea94489a6eb195fe04e63157601ff
Erkin Altuntas <erkin98@live.de>
2018-11-30 20:26:35 +0100

    Beschreibung der Pflanzen im SelectionPanel
    Wenn man mit der Maus über eine Pflanze geht wird eine Beschreibung sichtbar

---
+++ LCDProjekt/Assets/Scripts/DisplayDescription.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayDescription : MonoBehaviour {
+    public string myString;
+    public TextMeshProUGUI description;
+    public float fadeTime;
+    public bool displayInfo;
+
+	// Use this for initialization
+	void Start () {
+
+        description = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        description.color = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FadeText();
+        
+	}
+
+    void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+    {
+        if (displayInfo)
+        {
+            description.text = myString;
+            description.color = Color.Lerp(description.color, Color.white, fadeTime * Time.deltaTime);
+
+        }
+        else
+        {
+            description.color = Color.Lerp(description.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
