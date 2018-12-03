77c26cd053dbadaa483c594b34d2552391e96b31
Erkin Altuntas <erkin98@live.de>
2018-12-03 14:05:45 +0100

    button sounds

---
+++ LCDProjekt/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundScript : MonoBehaviour {
+
+
+    //zum Abspielen der Sounds
+    public AudioSource objectSound;
+
+    //Sounds
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    //spielt hoverSound wenn über Button gegangen wird
+    public void playHoverSound()
+    {
+        objectSound.PlayOneShot(hoverSound);
+    }
+
+    //spielt clickSound wenn auf Button gedrückt wird
+    public void playClickSound()
+    {
+        objectSound.PlayOneShot(clickSound);
+    }
+}
