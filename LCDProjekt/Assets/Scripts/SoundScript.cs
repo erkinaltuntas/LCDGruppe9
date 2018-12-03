09d10e05697365941c4908a14beb8f4b32c81585
Erkin Altuntas <erkin98@live.de>
2018-12-03 14:35:48 +0100

    soundscript

---
+++ LCDProjekt/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+    //speichert Sounds
+    public AudioSource audSource;
+
+    //Sounds
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void playHoverSound()
+    {
+        audSource.PlayOneShot(hoverSound);
+    }
+
+    public void playClickSound()
+    {
+        audSource.PlayOneShot(clickSound);
+    }
+}
