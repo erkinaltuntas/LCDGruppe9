2f9ed5be7169b8453e8074e912f29c94d7d3dd88
Vxu1 <vxu1@smail.uni-koeln.de>
2018-12-06 22:02:19 +0100

    Clock Animation hinzugefügt

---
+++ LCDProjekt/Assets/Scripts/ClockButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockButton : MonoBehaviour {
+
+    public Button harvestButton;
+    public GameObject clock;
+    public GameObject background;
+    public Animation clockAnimation;
+
+	// Use this for initialization
+	void Start () {
+
+        harvestButton.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void TaskOnClick()
+    {
+        clock.SetActive(true);
+        background.SetActive(true);
+        clockAnimation.Play("Clock");
+        
+
+    }
+
+    public void ClockAnimationEnd()
+    {
+        
+            clock.SetActive(false);
+            background.SetActive(false);
+        
+    }
+}
