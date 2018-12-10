3838ae9a64d4f5ff0a72a434acf4ac8770911228
cmeyerpi <cmeyerpi@smail.uni-koeln.de>
2018-12-10 14:14:41 +0100

    Namen Szene

---
+++ LCDProjekt/Assets/Countdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class Countdown : MonoBehaviour
+{
+    public int timeLeft = 60; //Seconds Overall
+    public Text countdown; //UI Text Object
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+    void Update()
+    {
+        countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+    }
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (timeLeft>0)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
