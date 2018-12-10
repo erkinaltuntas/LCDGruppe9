0899373d9035fbdfad4d9b2cd2c8313bd83784a7
Erkin Altuntas <erkin98@live.de>
2018-12-10 14:42:36 +0100

    code

--- LCDProjekt/Assets/Scripts/SceneLoad.cs
+++ LCDProjekt/Assets/Scripts/SceneLoad.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections;
+﻿/*************************************************************************** 
+* SceneLoad
+* Anwendung: Zur Steuerung der Szenenuebergaenge
+* ------------------- 
+* Zuletzt bearbeitet von: Erkin Altuntas
+* Datum der letzten Bearbeitung: 10.12.2018
+* Grund für letzte Bearbeitung: Kommentare/Code Pflege
+**************************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoad : MonoBehaviour {
 
-    //geht in die nächste Szene über
+    // Geht in die nächste Szene über
     public void loadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    //beendet das Spiel
+    // Beendet das Spiel
     public void quitGame()
     {
         Debug.Log("Spiel beendet");
