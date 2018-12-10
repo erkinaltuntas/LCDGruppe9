6356c77930cf75192a581c663264e76541c40384
cmeyerpi <cmeyerpi@smail.uni-koeln.de>
2018-12-10 15:39:07 +0100

    Timer Spring

--- LCDProjekt/Assets/Scripts/Countdown.cs
+++ LCDProjekt/Assets/Scripts/Countdown.cs
@@ -1,22 +1,34 @@
-﻿using System.Collections;
+﻿/*************************************************************************** 
+* Weather
+* Anwendung: Zur Defintion des Wetters sowie der Steuerung, ob sie von 
+* Duerre oder Frost betroffen sind
+*------------------- 
+* Zuletzt bearbeitet von: Cedric Meyer-Piening
+* Datum der letzten Bearbeitung: 10.12.2018
+* Grund für letzte Bearbeitung: Neu erstellt
+* **************************************************************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading;
+using TMPro;
+
 public class Countdown : MonoBehaviour
 {
     public int timeLeft = 60; //Seconds Overall
-    public Text countdown; //UI Text Object
+    public TextMeshProUGUI countdown; //UI Text Object
     void Start()
     {
         StartCoroutine("LoseTime");
-        Time.timeScale = 1; //Just making sure that the timeScale is right
+        Time.timeScale = 1; // Um sicher zu gehen, dass die Zeitskalierung stimmt
     }
     void Update()
     {
-        countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+        countdown.text = ("" + timeLeft); // Zeit im Canvas anzeigen
     }
-    //Simple Coroutine
+    // Countdown bis dieser abgelaufen ist
     IEnumerator LoseTime()
     {
         while (timeLeft>0)
