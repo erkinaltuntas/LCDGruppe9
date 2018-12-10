05ac0bcb2486ca6d3089ce41ce7c6e4b2559a48f
cmeyerpi <cmeyerpi@smail.uni-koeln.de>
2018-12-10 14:36:52 +0100

    Szenennamen

---
+++ LCDProjekt/Assets/Scripts/Feld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feld : MonoBehaviour {
+
+    public int id;
+    public string plantName = "";
+    //wie viel Prozent der Ernte erfolgreich war
+    public int successRate;
+    public bool shock;
+    public Plant plant;
+    Collider2D collider1;
+    public bool fieldIsHarvested;
+    
+    // Use this for initialization
+    void Start () {
+        collider1 = this.GetComponent<Collider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(plantName != "")
+        {
+            collider1.enabled = false; 
+        }
+	}
+}
