63affdbeadfeacfbaa72237ba62f73f6fcf21f68
Erkin Altuntas <erkin98@live.de>
2018-12-30 18:32:03 +0100

    Hintergrundmusik
    Läuft über alle Szenen, musik anpassbar.

---
+++ LCDProjekt/Assets/Scripts/Backgroundmusic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundmusic : MonoBehaviour {
+    static Backgroundmusic instance = null;
+
+    private void Awake()
+    {
+        // Steuert die Musik ueber die Level hinweg
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+            
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LCDProjekt/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicScript : MonoBehaviour {
+    private Backgroundmusic backgroundmusic;
+    public Button musicToggleButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+	// Use this for initialization
+	void Start () {
+        backgroundmusic = GameObject.FindObjectOfType<Backgroundmusic>();
+        UpdateIconAndVolume();
+	}
+
+    public void PauseMusic()
+    {
+        // PlayerPrefs aktualisieren
+        backgroundmusic.ToggleSound();
+        UpdateIconAndVolume();
+    }
+    
+    void UpdateIconAndVolume()
+    {
+        // Passe das Sprite und das Volumen an
+        if(PlayerPrefs.GetInt("Muted",0) == 0)
+        {
+            AudioListener.volume = 1;
+            musicToggleButton.GetComponent<Image>().sprite = musicOnSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            musicToggleButton.GetComponent<Image>().sprite = musicOffSprite;
+        }
+    }
+}
