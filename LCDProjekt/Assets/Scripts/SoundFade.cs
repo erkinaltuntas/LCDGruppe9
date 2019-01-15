f4a6d78ed923e847c2953fb3a05db5591320c51a
Vxu1 <vxu1@smail.uni-koeln.de>
2019-01-15 17:01:50 +0100

    Music Fade Ende

---
+++ LCDProjekt/Assets/Scripts/SoundFade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFade : MonoBehaviour {
+    
+
+
+   
+
+    public static IEnumerator FadeOut()
+    {
+        float startVolume = AudioListener.volume;
+
+        while (AudioListener.volume > 0)
+        {
+            AudioListener.volume -= 0.004f; 
+
+            yield return null;
+        }
+
+        AudioListener.volume = 0;
+    }
+    
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FadeOut());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
