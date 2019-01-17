02afa21c908861e73aafa12ad4fb9c2aa7f8a08f
Twiesch <wegwerfcobran@gmail.com>
2019-01-17 11:14:40 +0100

    Weather kommentiert

--- LCDProjekt/Assets/Scripts/Weather.cs
+++ LCDProjekt/Assets/Scripts/Weather.cs
@@ -1,7 +1,7 @@
 ﻿/*************************************************************************** 
 * Weather
-* Anwendung: Zur Defintion des Wetters sowie der Steuerung, ob sie von 
-* Duerre oder Frost betroffen sind
+* Anwendung: Zur Defintion des Wetters.  
+* Bestimmt ob in einer Jahreszeit Frost oder Dürre (oder beides) auftritt.
 *------------------- 
 * Zuletzt bearbeitet von: Thomas Wieschermann
 * Datum der letzten Bearbeitung: 20.12.2018
@@ -11,6 +11,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Zur Definition des Wetters. Bestimmt ob in einer Jahreszeit Frost oder Dürre (oder beides) auftritt,
+/// jedoch noch nicht ob eine bestimmte Pflanze davon betroffen ist.
+/// </summary>
 public class Weather : MonoBehaviour {
     public string seasonName;
     public int seasonNumber;
@@ -21,7 +25,11 @@ public class Weather : MonoBehaviour {
 
     public int random1, random2;
 
-    // Use this for initialization
+    /// <summary>
+    /// Bei Start werden zunächst zwei zufällige Integer-Werte zwischen 0 und 9 festgelegt.
+    /// Sukzessiv werden mit diesen beiden zufälligen Integer-Werten die beiden Methoden isFrost und isDrought aufgerufen.
+    /// Dabei wird random1 benutzt um festzulegen ob es Frost gibt und random2 um festzulegen ob es Dürre gibt.
+    /// </summary>
     void Start () {
 
         random1 = Random.Range(0, 10);
@@ -31,6 +39,13 @@ public class Weather : MonoBehaviour {
 
     }
 
+    /// <summary>
+    /// Bestimmt ob in einer bestimmten Jahreszeit Frost bei Pflanzen vorkommen kann.
+    /// Dabei wird der Zufallswert random1 mit der Frostwahrscheinlichkeit der Jahreszeit verglichen.
+    /// Sollte der Zufallswert kleiner gleich dem Wahrscheinlicheitswert der Jahreszeit sein, ist für alle Pflanzen in der Jahreszeit
+    /// Frost möglich.
+    /// </summary>
+    /// <returns>Wahrheitswert der angibt ob Pflanzen von Frost in dieser Jahreszeit betroffen sein koennen</returns>
     public bool isFrost()
     {
         if (random1 <= this.frostProb * 10)
@@ -40,6 +55,13 @@ public class Weather : MonoBehaviour {
         return false;
     }
 
+    /// <summary>
+    /// Bestimmt ob in einer bestimmten Jahreszeit Duerre bei Pflanzen vorkommen kann.
+    /// Dabei wird der Zufallswert random1 mit der Duerrewahrscheinlichkeit der Jahreszeit verglichen.
+    /// Sollte der Zufallswert kleiner gleich dem Wahrscheinlicheitswert der Jahreszeit sein, ist für alle Pflanzen in der Jahreszeit
+    /// Duerre möglich.
+    /// </summary>
+    /// <returns>Wahrheitswert der angibt ob Pflanzen von Duerre in dieser Jahreszeit betroffen sein koennen</returns>
     public bool isDrought()
     {
         if (random2 <= this.droughtProb * 10)
