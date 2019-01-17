ab11b5520c826a64afebac42184981e58b5d6c6c
Twiesch <wegwerfcobran@gmail.com>
2019-01-17 15:57:20 +0100

    SoundFade Kommentare

--- LCDProjekt/Assets/Scripts/SoundFade.cs
+++ LCDProjekt/Assets/Scripts/SoundFade.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections;
+﻿/*************************************************************************** 
+* SoundFade
+* Anwendung: SoundFade am Ende des Spiels
+*------------------- 
+* Zuletzt bearbeitet von: Thomas Wieschermann
+* Datum der letzten Bearbeitung: 17.01.2019
+* Grund für letzte Bearbeitung: Kommentare/Code Pflege
+**************************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Ermöglicht das Abklingen des InGame-Sounds am Ende des Spiels
+/// </summary>
 public class SoundFade : MonoBehaviour {
     
-
-
-   
-
+    /// <summary>
+    /// Solange die Lautstärke groesser als null ist, wird 
+    /// pro Durchlaufen der Schleife die <c>AudioListener.volume</c> um den 
+    /// festen Wert 0.004 verringert. Die Methode läuft solange bis
+    /// <c>Audiolistener.volume</c> den Wert null erreicht hat.
+    /// </summary>
+    /// <returns></returns>
     public static IEnumerator FadeOut()
     {
         float startVolume = AudioListener.volume;
@@ -23,13 +37,19 @@ public class SoundFade : MonoBehaviour {
     }
     
 
-    // Use this for initialization
+    /// <summary>
+    /// StartCoroutine und Update erlauben es zusammen hier FadeOut einmal pro 
+    /// in-Game frame aufzurufen.
+    /// </summary>
     void Start () {
         StartCoroutine(FadeOut());
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    /// <summary>
+    /// StartCoroutine und Update erlauben es zusammen hier FadeOut einmal pro 
+    /// in-Game frame aufzurufen.
+    /// </summary>
+    void Update () {
     }
 }
